191d3e79a8aa5329fdd74c0e7f11840a9b87ed3b
Gilney N. Mathias <gilney_salvo@hotmail.com>
2018-02-26 12:31:45 -0300

    Separar o header das spreadsheets para ser reutilizado em outras páginas

--- resources/views/spreadsheets/create.blade.php
+++ resources/views/spreadsheets/create.blade.php
@@ -1,13 +1,7 @@
 @extends('layout.layout')
 
 @section('header')
-  <header class="container section">
-    <h4 class="header center teal-text text-lighten-2">PROGRAMA DE QUALIDADE EM RADIOTERAPIA - PQRT</h4>
-    <div class="row center">
-    <p class="header col s12">PLANILHA DE INTERPOLAÇÃO PARA O CÁLCULO DE k<sub>Q,Q0</sub> E k<sub>Q,Qint</sub> E DECAIMENTO DO C0-60</p>
-    </div>
-  </header>
-  <div class="divider"></div>
+  @include('layout.partials.spreadsheet-header')
 @endsection
 
 @section('content')
